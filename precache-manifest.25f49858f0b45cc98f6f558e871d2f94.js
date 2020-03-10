4f12b63300b9dbd7cb89df1c0e4863cef28e82ed
grunfes <grunfes@yahoo.com>
2020-03-10 11:07:06 +0100

    Updates

---
+++ precache-manifest.25f49858f0b45cc98f6f558e871d2f94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681f811b0cc44ab3d24a171d4b53d205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f86371ecd60609fc21ee",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "832d82b409cf4de7f642",
+    "url": "/static/css/main.bd613b47.chunk.css"
+  },
+  {
+    "revision": "f86371ecd60609fc21ee",
+    "url": "/static/js/2.0fdadb66.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.0fdadb66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832d82b409cf4de7f642",
+    "url": "/static/js/main.bdcd6a41.chunk.js"
+  },
+  {
+    "revision": "7f31f04669a7e0c04c51",
+    "url": "/static/js/runtime-main.59d46d1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
